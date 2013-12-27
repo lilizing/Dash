8927e8cfd75a716740507f62cad7ad89aa815cb6
lili <lili@kdanmobile.com>
2013-12-27 09:56:15 +0800

    UIView+Util定义与整理
    Signed-off-by: lili <lili@kdanmobile.com>

---
+++ src/UIView+Util.h
@@ -0,0 +1,63 @@
+//
+//  UIView+Util.h
+//  WordDocPlus
+//
+//  Created by lili on 13-12-27.
+//  Copyright (c) 2013年 kdanmobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Util)
+
++ (id)viewFromNibNamed:(NSString *)nibName;
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil;
+
+- (UIImage*)screenshot;
+- (UIImage*)screenshotWithOptimization:(BOOL)optimized;
+
+@property (nonatomic, assign) CGPoint origin;
+@property (nonatomic, assign) CGSize size;
+
+@property (nonatomic, assign) CGFloat left;
+@property (nonatomic, assign) CGFloat right;
+@property (nonatomic, assign) CGFloat top;
+@property (nonatomic, assign) CGFloat bottom;
+
+@property (nonatomic, assign) CGFloat centerX;
+@property (nonatomic, assign) CGFloat centerY;
+
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+
+@property (nonatomic, readonly) CGFloat screenX;
+@property (nonatomic, readonly) CGFloat screenY;
+
+@property (nonatomic, readonly) CGFloat screenViewX;
+@property (nonatomic, readonly) CGFloat screenViewY;
+
+@property (nonatomic, readonly) CGRect screenFrame;
+
+@property (nonatomic, readonly) CGFloat orientationWidth;
+@property (nonatomic, readonly) CGFloat orientationHeight;
+
+- (void)centerInRect:(CGRect)rect;
+- (void)centerVerticallyInRect:(CGRect)rect;
+- (void)centerHorizontallyInRect:(CGRect)rect;
+
+- (void)centerInSuperView;
+- (void)centerVerticallyInSuperView;
+- (void)centerHorizontallyInSuperView;
+
+- (void)centerHorizontallyBelow:(UIView *)view padding:(CGFloat)padding;
+- (void)centerHorizontallyBelow:(UIView *)view;
+
+- (UIView*)descendantOrSelfWithClass:(Class)cls;
+- (UIView*)ancestorOrSelfWithClass:(Class)cls;
+
+- (void)removeAllSubviews;
+
+- (void)setTapActionWithBlock:(void (^)(void))block;
+- (void)setLongPressActionWithBlock:(void (^)(void))block;
+
+@end
